3e821a763b7c0b76ad2f693c6f7dc5d3315ad54b
Sujal Tamrakar <sanskarsujaltamrakar2903@gmail.com>
2022-10-06 22:32:57 +0530

    day3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:e_learn/home_page.dart';
+import 'package:e_learn/pages/home_page.dart';
+import 'package:e_learn/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,12 +7,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.red),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  
 
   final int days = 30;
   final String name = "IndoriCoder";
